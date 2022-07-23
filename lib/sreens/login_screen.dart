980b7d3fe84dbc8376f1237359613ac47ce044a1
rafaelcastrobr <rafaelcastrobr@gmail.com>
2022-07-23 17:23:41 -0300

    criação pagina login custom colors

--- lib/sreens/login_screen.dart
+++ lib/sreens/login_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+
+import 'package:dart_flutter_test/variables/custom_colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
@@ -13,10 +16,14 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   Color topColor = Colors.blue;
   Color bottonColor = Color.fromARGB(255, 177, 203, 224);
+
+  bool _continueConnected = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 50),
         decoration: BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topCenter,
@@ -28,16 +35,46 @@ class _LoginScreenState extends State<LoginScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            Padding(
+              padding: EdgeInsets.only(bottom: 15),
+              child: Image.asset(
+                "assets/ico-academia.gif",
+                height: 100,
+              ),
+            ),
             Text(
               "Entrar",
               textAlign: TextAlign.center,
               style: TextStyle(
                 letterSpacing: 5,
-                color: Colors.white,
+                color: CustomColor().activePrimaryColor,
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
               ),
-            )
+            ),
+            Form(
+              child: Column(
+                children: [
+                  TextFormField(
+                      decoration: InputDecoration(
+                    labelText: "Digite um título",
+                    labelStyle: TextStyle(color: Colors.white),
+                    prefixIcon: MouseRegion(
+                      cursor: SystemMouseCursors.click,
+                      child: GestureDetector(
+                        child: Icon(
+                          Icons.search,
+                          color: Colors.white,
+                        ),
+                        onTap: () {
+                          print("DE NOVO");
+                        },
+                      ),
+                    ),
+                  )),
+                ],
+              ),
+            ),
           ],
         ),
       ),
---
+++ lib/variables/custom_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  Color activePrimaryColor = Colors.red;
+
+  Color getActivePrimaryColor() {
+    return activePrimaryColor;
+  }
+}
